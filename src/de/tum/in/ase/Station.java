4dca8cfddf4a6093d5abdb7543066e09f301afe6
Alp Kaan Aksu <alpkaan.aksu@tum.de>
2022-10-30 20:39:14 +0100

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Station.java
@@ -0,0 +1,11 @@
+package de.tum.in.ase;
+
+public class Station {
+    // TODO: Constructor
+
+    // TODO: Getters and setters
+
+    // TODO: acceptTrain
+
+    // TODO: equals (checks if the names of two stations are the same, returns true if that is the case)
+}
---
+++ src/de/tum/in/ase/Train.java
@@ -0,0 +1,19 @@
+package de.tum.in.ase;
+
+public class Train {
+
+    // TODO: Constructor
+
+    // TODO: Getters and setters
+
+    // TODO: Board and Deboard
+
+    // TODO: Delay train
+
+    // TODO: Finish shift
+
+    // TODO: Choo choo!
+
+    // TODO: toString
+
+}
