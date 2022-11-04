5ae08e9697dd513abc61f0b97522d48b03da28cf
ge32goy <hanyin.chen@tum.de>
2022-11-04 09:45:10 +0100

    commit

--- src/de/tum/in/ase/Station.java
+++ src/de/tum/in/ase/Station.java
@@ -1,11 +1,51 @@
 package de.tum.in.ase;
 
 public class Station {
+    private String name;
+    private int trains = 0;
+    private int totalPassengers = 0;
     // TODO: Constructor
+
+    public Station(String name) {
+        this.name = name;
+    }
 
     // TODO: Getters and setters
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTrains() {
+        return trains;
+    }
+
+    public void setTrains(int trains) {
+        this.trains = trains;
+    }
+
+    public int getTotalPassengers() {
+        return totalPassengers;
+    }
+
+    public void setTotalPassengers(int totalPassengers) {
+        this.totalPassengers = totalPassengers;
+    }
+
+
     // TODO: acceptTrain
+    public void acceptTrain(Train train) {
+        this.trains += 1;
+        this.totalPassengers += 1;
+
+    }
 
     // TODO: equals (checks if the names of two stations are the same, returns true if that is the case)
+    public boolean equals(Station station) {
+
+    }
 }
--- src/de/tum/in/ase/Train.java
+++ src/de/tum/in/ase/Train.java
@@ -1,19 +1,80 @@
 package de.tum.in.ase;
 
 public class Train {
-
+    private int vehicleNumber;
+    private int passengers = 0;
+    private boolean moving = false;
+    private Station currentStation;
     // TODO: Constructor
-
+    public Train(int vehicleNumber, Station currentStation) {
+        this.vehicleNumber = vehicleNumber;
+        this.currentStation = currentStation;
+    }
     // TODO: Getters and setters
 
+    public int getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public void setVehicleNumber(int vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public Station getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(Station currentStation) {
+        this.currentStation = currentStation;
+    }
+
     // TODO: Board and Deboard
+    public void boardPassenger() {
+        this.passengers += 1;
+    }
+
+    public void deboardPassenger() {
+        this.passengers -= 1;
+    }
+
 
     // TODO: Delay train
+    public void delayTrain() {
+        setMoving(false);
+        setPassengers(getPassengers() / 2);
+    }
 
     // TODO: Finish shift
+//    TODO: need to change to Station class
+    public void finishShift() {
+        setCurrentStation("Munich Central Station");
+        setPassengers(0);
+    }
 
     // TODO: Choo choo!
+    public void choochoo() {
+        System.out.println("Choo choo, here comes the train!");
+    }
 
     // TODO: toString
+    public String toString() {
+        return String.format("The train with the vehicle number %d is currently in the station %s. There are currently %d passengers on board.%n", vehicleNumber, station, passengers);
+    }
 
 }
